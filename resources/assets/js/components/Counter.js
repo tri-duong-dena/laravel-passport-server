a8a1ce6c61c6e08438e261e5242823093014a71d
tri.duong <tri.duong@dena.com>
2016-12-07 17:32:43 +0700

    [Redux] Call API from React component

--- resources/assets/js/components/Counter.js
+++ resources/assets/js/components/Counter.js
@@ -16,6 +16,19 @@ class Counter extends Component {
     setTimeout(this.props.onIncrement, 1000)
   }
 
+  componentDidMount() {
+    console.log('Component mounted' + Laravel.csrfToken)
+    fetch('/api/user', {
+    headers: {
+      'Authorization': 'Bearer ' + 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjY0ZDBkNWYxOWRjNzA2MzM5ZjI0OGQwYWY1OTE5ZmQwYzU1NDM4M2ZlZDNkNzA0ZGM4NTMzNGRlYmRiN2I2NzBkZTk2NDMxNWI0NzQ0NTE3In0.eyJhdWQiOiIzIiwianRpIjoiNjRkMGQ1ZjE5ZGM3MDYzMzlmMjQ4ZDBhZjU5MTlmZDBjNTU0MzgzZmVkM2Q3MDRkYzg1MzM0ZGViZGI3YjY3MGRlOTY0MzE1YjQ3NDQ1MTciLCJpYXQiOjE0ODA2Njc2MTcsIm5iZiI6MTQ4MDY2NzYxNywiZXhwIjoxNTEyMjAzNjE3LCJzdWIiOiIxIiwic2NvcGVzIjpbXX0.aXhHesN8kjUVi1GzxirswMO66dtfaFvszHGOzLzO3FvZMufWUHk-gSl5kKTRXQSEClRJr4d7xGj15DIJLVj0-IU-HEsAYoGmOp0VItxc2bPrsmKSnmowhItu4_VmIHbdvaVW7GZ5enNO5vG9gDe5FQOmk-nI1-Z5sbKDEDuHbS0bacCgkL1vD1lz6StGlcO1K4pCTEXqW6yO84-x4I-XM8Vbuo7CPVNM8dP4th3ZPi8wXwlZggaixEGH6kh_YtRVmrjivc-eJPT5hGS_WaMCBVOD6xFVnQM-Ktnw_YIfrB3uokaiFv9Tk-rLzMXu-xKvatnNvKNfZFLnrXOW8FK-UFvGf_KI3Ifnu1xIaZWnU4_4GAulSp6oAIlUTCjL-QQEVd0knqIzYKqp-HWPvHLC5zh9lWBFx5ZfhnsWNqPyN_oVtq3xLeq_g9--dZEH4R6EHcc_QM414UNp6Nc-Z4xMWNjss8bgbC4vimLtgTnhyjetjScC4PlkdKohVb_6d5QQTm7gWyrDkcWzynMuRqlQfEtQEecOiUKbXYXNNdVLfEVDKqejx4FU7DKfpfP9RYw3YhgzIED1phBFHNePZfxo-1tUyzeJkAgbR2n68SXM4H6roXgswoP-XaM4MIHa6sUHSqC0qrffQ-mo9vFNFGhyaKzPCmij15-B2aJWv8QKsJs'
+    }})
+        .then((result) => result.json())
+        .then((resultJson) => {
+            console.log(resultJson)
+            console.log(resultJson.email)
+        })
+  }
+
   render() {
     const { value, onIncrement, onDecrement } = this.props
     return (
