debceac9c94c20e391e2f3c86bab2de1344bd3b4
tri.duong <tri.duong@dena.com>
2016-12-07 18:13:25 +0700

    [Redux] Use cookie and csrf token instead of JWT token

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -25,6 +25,7 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \App\Http\Middleware\Locale::class,
+            \Laravel\Passport\Http\Middleware\CreateFreshApiToken::class,
         ],
 
         'admin' => [
